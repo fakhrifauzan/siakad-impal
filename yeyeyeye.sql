f4429e2febd9fc5df0e206fa3bc7476cbe64fb75
Fakhri Fauzan <fazan697@gmail.com>
2017-09-14 07:12:26 +0700

    updateDB

--- yeyeyeye.sql
+++ yeyeyeye.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Sep 13, 2017 at 06:51 PM
--- Server version: 10.0.31-MariaDB-0ubuntu0.16.04.2
--- PHP Version: 7.0.23-1+ubuntu16.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2017 at 02:11 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,8 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `lalalala`
+-- Database: `yeyeyeye`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `config`
+--
+
+CREATE TABLE `config` (
+  `config` varchar(50) NOT NULL,
+  `value` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`config`, `value`) VALUES
+('status_reg', 'Aktif'),
+('tahun_ajar', '1516/2');
 
 -- --------------------------------------------------------
 
@@ -46,9 +67,85 @@ INSERT INTO `dosen` (`kode_dosen`, `nama_dosen`, `fakultas`, `status`, `username
 ('REZ', 'Reza Aditama', 'Industri Kreatif', 'Tetap', 'rez', 'rez'),
 ('ZZZ', 'Zulaiha', 'Informatika', 'Tetap', 'zzz', 'zzz');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `kode_kelas` varchar(10) NOT NULL,
+  `fakultas` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `doswal` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`kode_kelas`, `fakultas`, `prodi`, `doswal`) VALUES
+('IF-39-10', 'Informatika', 'S1 Teknik Informatika', 'AAA'),
+('MB-40-09', 'Manajemen', 'S1 Manajemen', 'ZZZ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `fakultas` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL,
+  `kelas` varchar(10) NOT NULL,
+  `tahun_masuk` varchar(5) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `fakultas`, `prodi`, `kelas`, `tahun_masuk`, `username`, `password`) VALUES
+(111, 'Adam Budi', 'Informatika', 'S1 Teknik Informatika', 'IF-39-10', '2015', '111', '111'),
+(222, 'Rahayu', 'Informatika', 'S1 Teknik Informatika', 'IF-39-10', '2014', '222', '222');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matkul`
+--
+
+CREATE TABLE `matkul` (
+  `kode_matkul` varchar(10) NOT NULL,
+  `nama_matkul` varchar(50) NOT NULL,
+  `sks` int(2) NOT NULL,
+  `fakultas` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `matkul`
+--
+
+INSERT INTO `matkul` (`kode_matkul`, `nama_matkul`, `sks`, `fakultas`) VALUES
+('DAP', 'Dasar Algoritma dan Pemograman', 3, 'Informatika'),
+('MPB', 'Manajemen Pemasaran Bisnis', 2, 'Manajemen'),
+('NIR', 'Nirmana', 4, 'Industri Kreatif'),
+('PBO', 'Pemograman Berorientasi Objek', 3, 'Informatika'),
+('STD', 'Struktur Data', 3, 'Informatika');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`config`);
 
 --
 -- Indexes for table `dosen`
@@ -56,6 +153,26 @@ INSERT INTO `dosen` (`kode_dosen`, `nama_dosen`, `fakultas`, `status`, `username
 ALTER TABLE `dosen`
   ADD PRIMARY KEY (`kode_dosen`),
   ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`kode_kelas`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `matkul`
+--
+ALTER TABLE `matkul`
+  ADD PRIMARY KEY (`kode_matkul`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
